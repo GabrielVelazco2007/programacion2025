21ac1d0625094b703e5b6b2ae5316278044f6928
Luciano Velazco <gabrielvelazco277@gmail.com>
2025-04-03 14:02:34 -0300

    concadenacion

---
+++ concatenacion.php
@@ -0,0 +1,16 @@
+<?php 
+
+$emisora = 'Radio Disney';
+echo "Me gusta escuchar $emisora";
+?>
+
+
+/*comillas simple:
+Comillas simples ('): Se usan para cadenas de texto fijas, 
+donde todo lo que pongas dentro de ellas se va a mostrar tal cual. 
+No se interpretan variables ni caracteres especiales.
+
+comillas dobles:Comillas dobles ("): 
+Se usan cuando quieres que PHP lea las variables
+ dentro de la cadena y las reemplace con su valor real. 
+También permite ciertos caracteres especiales como saltos de línea
